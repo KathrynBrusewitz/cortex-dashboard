85a90b270f21182f95aa62b89143934ac3043ac3
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-04-20 10:05:26 -0700

    podcast pages: create, edit, view forms

--- src/components/podcasts/CreatePodcast.js
+++ src/components/podcasts/CreatePodcast.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const CreatePodcast = () => (
-  <div>
-    <h1>CreatePodcast</h1>
-  </div>
-);
+import PodcastForm from './PodcastForm';
 
-export default CreatePodcast;
+import { podcastActions } from '../../actions';
+
+class CreatePodcast extends Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(options = {}) {
+    // console.log(this.props.formValues);
+    // this.props.createPodcast(this.props.formValues);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create New Podcast</h1>
+        <PodcastForm initialValues={{}} onSubmit={this.onFormSubmit} />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  // ...podcastActions,
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(CreatePodcast);
