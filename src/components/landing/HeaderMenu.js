8ac690740ba846d4f66ea58faf8441adb43d943d
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-05-27 00:28:12 -0700

    clean up

--- src/components/landing/HeaderMenu.js
+++ src/components/landing/HeaderMenu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Menu, Icon } from 'antd';
+import { Menu } from 'antd';
 
 const items = [
   { icon: 'api', name: 'Home', route: '/' },
